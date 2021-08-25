5ab3ac2a709b6f05582d4ed0b7ab409b0f27aedd
Donald Hughes <donald.hughes@gmail.com>
2021-08-24 21:09:45 -0500

    Handled Apple HEIC format for photos from iOS devices.

--- ocr.js
+++ ocr.js
@@ -2,12 +2,32 @@ const fileInput = document.getElementById('image-picker');
 fileInput.addEventListener('change', (e) => doOcrOnFile(e.target.files));
 
 
-function doOcrOnFile(files) {
+async function doOcrOnFile(files) {
 
-	document.getElementById('loader').style.display = "block";
+	var fileName = files[0].name;
+    var fileNameExt = fileName.substr(fileName.lastIndexOf('.') + 1);
 
+	document.getElementById('loader').style.display = 'block';
+
+    if(fileNameExt.toLowerCase() == "heic") {
+
+        let blobURL = URL.createObjectURL(files[0]);
+        let blobRes = await fetch(blobURL);
+        let blob = await blobRes.blob();
+        let conversionResult = await heic2any({ blob, toType: "image/jpg" });
+        doOCR(conversionResult);
+
+    } else { 
+
+    	doOCR(files[0]);
+	}
+
+}
+
+
+function doOCR(file) {
 	const worker = Tesseract.createWorker({
-			logger: m => console.log(m)
+					logger: m => console.log(m)
 	});
 
 	Tesseract.setLogging(true);
@@ -16,12 +36,13 @@ function doOcrOnFile(files) {
 	  await worker.load();
 	  await worker.loadLanguage('eng');
 	  await worker.initialize('eng');
-	  const { data: { text } } = await worker.recognize(files[0]);
+  	  const { data: { text } } = await worker.recognize(file);
+  		//const { data: { text } } = await worker.recognize(ocrTarget);
+
 	  console.log(text);
 	  document.getElementById('ocr-contents').innerHTML = text;
 	  await worker.terminate();
 	  document.getElementById('loader').style.display = "none";
 	  document.getElementById('listen-button').classList.toggle("disabled");
 	})();
-
 }
